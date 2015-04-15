68b31366c0edddc391cc48c882403ab008772b8f
hanshenrik <hh.gronsleth@gmail.com>
2015-04-15 17:00:47 +0100

    printing everywhere, create child processes in slaves

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 #define MSGsize 100
 
 int main(int argc, char **argv)
-{ int my_rank, NP;
+{ // MPI variables
+  int my_rank, NP;
   int source, dest;
   int tag = 50;
   int length;
@@ -15,20 +16,31 @@ int main(int argc, char **argv)
   MPI_Init(&argc, &argv); // argc and argv passed by address to all MPI processes
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // returns taskID=rank of calling process
   MPI_Comm_size(MPI_COMM_WORLD, &NP); // returns total no of MPI processes available
+
+  // Shared bank variables
   
   if (my_rank != 0) // slave
-  { MPI_Get_processor_name(name, &length);
-    sprintf(message, "From process %d on processor %s", my_rank, name);
+  { // Branch variables
+    
+
+    MPI_Get_processor_name(name, &length);
+    sprintf(message, "Slave: From process %d on processor %s", my_rank, name);
     dest = 0;
     MPI_Send(message, strlen(message) + 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
   }
   else // master
-  { for (source = 1; source < NP; source++)
+  { // Head Office variables
+
+
+    for (source = 1; source < NP; source++)
     { MPI_Recv(message, MSGsize, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
-      printf("%s\n", message);
+      printf("Server: %s\n", message);
     }
   }
   
   MPI_Finalize();
   return 0;
-} 
+}
+
+
+//int deposit()
