5b99a9cf72911f7ca86e493871d393a971a40bcb
hanshenrik <hh.gronsleth@gmail.com>
2015-04-16 15:18:19 +0100

    print forever every 1 second in slave child processes

--- main.c
+++ main.c
@@ -6,6 +6,14 @@
 #define MSGsize 100
 #define MASTER  0
 
+// stuff to use
+// MPI_Is_thread_main
+// MPI_Initialized
+// MPI_Wait
+// MPI_Waitall
+// MPI_Waitany
+
+
 int main(int argc, char **argv)
 { // MPI variables
   int my_rank, NP;
@@ -23,14 +31,14 @@ int main(int argc, char **argv)
 
   int i;
   for (i = 0; i < argc; i++) {
-    printf("main: argc = %d, argv[%d] = %s\n", argc, i, argv[i]);
+    // printf("main: argc = %d, argv[%d] = %s\n", argc, i, argv[i]);
   }
   
   if (my_rank == MASTER) // master
   { // Head Office variables
 
     printf("HO: started\n");
-    printf("HO: MPI_MAX_PROCESSOR_NAME = %d\n", MPI_MAX_PROCESSOR_NAME);
+    // printf("HO: MPI_MAX_PROCESSOR_NAME = %d\n", MPI_MAX_PROCESSOR_NAME);
     
     for (source = 1; source < NP; source++)
     { MPI_Recv(message, MSGsize, MPI_CHAR, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
@@ -44,7 +52,7 @@ int main(int argc, char **argv)
     int number_of_accounts = 3;
     int account_status = 0, wpid, i;
 
-    printf("BO: started\n");
+    // printf("BO: started\n");
 
     MPI_Get_processor_name(name, &length);
     printf("BO: name = %s, length = %d\n", name, length);
@@ -58,14 +66,18 @@ int main(int argc, char **argv)
       }
       if (pid == 0) {
         printf("BO%d: pid == 0, i = %d\n", my_rank, i);
+        while (1) {
+          sleep(1);
+          printf("%d_%d\n", my_rank, i);
+        }
         return 0;
       } else {
-        printf("BO: pid != 0\n");
+        // printf("BO: pid != 0\n");
       }
     }
 
     // wait for all children to exit
-    while ( (wpid = wait(&account_status) ) > 0) {}
+    // while ( (wpid = wait(&account_status) ) > 0) {}
 
     // wait for my_tank seconds to distinguish slaves in output
     sleep(my_rank);
@@ -82,7 +94,18 @@ int main(int argc, char **argv)
   return 0;
 }
 
-int deposit(int account_number, int amount)
+int deposit(int account_number, double amount)
 {
   return 0;
+}
+
+int withdraw(int account_number, double amount)
+{
+  // TODO: check balance
+  return 0;
+}
+
+int transfer(int from_account_number, int to_account_number, double amount)
+{
+  // TODO: check balance
 }
