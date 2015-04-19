94fe9cf8aa4547aea395d45c15a6904797e02106
hanshenrik <hh.gronsleth@gmail.com>
2015-04-19 23:03:51 +0100

    start individual process per account, with balance set to db value

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #define QUERY_SIZE 500
 #define MASTER  0
 
+double balance = 100.00;
+
 // stuff to use
 // MPI_Is_thread_main
 // MPI_Initialized
@@ -15,6 +17,8 @@
 // MPI_Waitall
 // MPI_Waitany
 
+
+// gSOAP with simple HTTP (Proxy Basic?) Authentication, don't have time to bother with HTTPS/OpenSSL, but mention in report!
 
 static int callback(void *pswd, int argc, char **argv, char **azColName){
   int i;
@@ -25,7 +29,7 @@ static int callback(void *pswd, int argc, char **argv, char **azColName){
   printf("CB: dbPassword = %s\n", dbPassword);
   if (strcmp(inputPassword, dbPassword) == 0) {
     printf("CB: match!\n");
-    return 0;
+    return SQLITE_OK;
   } else {
     printf("CB: no match!\n");
   }
@@ -69,8 +73,8 @@ int main(int argc, char **argv)
 
     printf("HO: started\n");
     MPI_Get_processor_name(name, &length);
-    printf("HO: name = %s\n", name);
-    printf("HO: my_rank = %d\n", my_rank);
+    printf("HO: name = %s, my_rank = %d\n", name, my_rank);
+    printf("HO: balance = %f\n", balance);
 
     // authenticate user against users.db
     rc = sqlite3_open("users.db", &db); // change to v2 for read-only flags etc.
@@ -89,7 +93,6 @@ int main(int argc, char **argv)
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
       sqlite3_free(zErrMsg);
     }
-
     else { // user was valid, continue
       printf("HO: user authenticated!\n");
 
@@ -109,8 +112,8 @@ int main(int argc, char **argv)
         fprintf(stderr, "Failed..\n");
         return 1;
       }
-      sqlite3_reset(statement); // not sure if this is correct usage
       sqlite3_finalize(statement);
+
       sprintf(message, "%s | %d | %d | %f", username, account_id, operation, amount);
       MPI_Send(message, strlen(message) + 1, MPI_CHAR, branch, tag, MPI_COMM_WORLD);
     }
@@ -128,33 +131,77 @@ int main(int argc, char **argv)
     int number_of_accounts = 3;
     int account_status = 0, wpid, i;
 
-    // printf("BO: started\n");
-
     MPI_Get_processor_name(name, &length);
-    printf("BO: name = %s, length = %d\n", name, length);
+    printf("BO: name = %s, my_rank = %d\n", name, my_rank);
 
+    // get number of accounts to start
+    rc = sqlite3_open("accounts.db", &db); // change to v2 for read-only flags etc.
+
+    if (rc) {
+      fprintf(stderr, "HO: Can't open database: %s\n", sqlite3_errmsg(db));
+      sqlite3_close(db);
+      return(1);
+    }
+
+    // find all account ids for this branch
+    sqlite3_stmt *statement;
+    sprintf(query, "SELECT id, balance FROM accounts WHERE branch = %d", my_rank);
+    sqlite3_prepare_v2(db, query, strlen(query) + 1, &statement, NULL);
+    // TODO: Bind values to host parameters using the sqlite3_bind_*() interfaces.
+    while (1) {
+      int row = sqlite3_step(statement);
+      if (row == SQLITE_ROW) {
+        int id;
+        id = sqlite3_column_int(statement, 0);
+        balance = sqlite3_column_double(statement, 1);
+        pid = fork();
+        if (pid < 0 ) {
+          printf("BO: pid < 0, fork failed!\n");
+          continue;
+        }
+        if (pid == 0) {
+          printf("BO%d: pid == 0, id = %d, balance = %f\n", my_rank, id, balance);
+          // while (1) {
+          //   sleep(1);
+          //   printf("%d_%d\n", my_rank, i);
+          // }
+          return 0;
+        } else {
+          // printf("BO: pid != 0\n");
+        }
+      } else if (row == SQLITE_DONE) {
+        printf("DONE\n");
+        break;
+      } else {
+        fprintf(stderr, "Failed..\n");
+        return 1;
+      }
+    }
+    sqlite3_finalize(statement);
+    sqlite3_close(db);
+
+    // start child processes (bank accounts)
+    // for (i = 0; i < number_of_accounts; i++)
+    // { pid = fork();
+    //   if (pid < 0 ) {
+    //     printf("BO: pid < 0, fork failed!\n");
+    //     continue;
+    //   }
+    //   if (pid == 0) {
+    //     printf("BO%d: pid == 0, i = %d\n", my_rank, i);
+    //     // while (1) {
+    //     //   sleep(1);
+    //     //   printf("%d_%d\n", my_rank, i);
+    //     // }
+    //     return 0;
+    //   } else {
+    //     // printf("BO: pid != 0\n");
+    //   }
+    // }
+    
     if (my_rank == 2) {
       MPI_Recv(message, MSG_SIZE, MPI_CHAR, MASTER, tag, MPI_COMM_WORLD, &status);
       printf("BO2 recv from master: %s\n", message);
-    }
-
-    // start child processes (bank accounts)
-    for (i = 0; i < number_of_accounts; i++)
-    { pid = fork();
-      if (pid < 0 ) {
-        printf("BO: pid < 0, fork failed!\n");
-        continue;
-      }
-      if (pid == 0) {
-        printf("BO%d: pid == 0, i = %d\n", my_rank, i);
-        // while (1) {
-        //   sleep(1);
-        //   printf("%d_%d\n", my_rank, i);
-        // }
-        return 0;
-      } else {
-        // printf("BO: pid != 0\n");
-      }
     }
 
     // wait for all children to exit
@@ -176,7 +223,34 @@ int main(int argc, char **argv)
 }
 
 double getBalance(int account_number) {
-  return 0.0;
+  // rc = sqlite3_open("accounts.db", &db); // change to v2 for read-only flags etc.
+
+  // if (rc) {
+  //   fprintf(stderr, "HO: Can't open database: %s\n", sqlite3_errmsg(db));
+  //   sqlite3_close(db);
+  //   return(1);
+  // }
+  
+  // sqlite3_stmt *statement;
+  // sprintf(query, "SELECT balance FROM accounts WHERE id = '%s'", account_number);
+  // sqlite3_prepare_v2(db, query, strlen(query) + 1, &statement, NULL);
+  // // TODO: Bind values to host parameters using the sqlite3_bind_*() interfaces.
+
+  // int row = sqlite3_step(statement);
+  // printf("row = %d\n", row);
+  // if (row == SQLITE_ROW) {
+  //   double balance;
+  //   balance = sqlite3_column_int(statement, 0);
+  // } else if (row == SQLITE_DONE) {
+  //   break;
+  // } else {
+  //   fprintf(stderr, "Failed..\n");
+  //   return 1;
+  // }
+  // sqlite3_finalize(statement);
+  // sqlite3_close(db);
+
+  return balance;
 }
 
 int deposit(int account_number, double amount)
