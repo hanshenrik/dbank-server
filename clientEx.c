5d47f1e5d2d4dc05a4cad78960c2c463d88d88cd
hanshenrik <hh.gronsleth@gmail.com>
2015-04-21 01:56:35 +0100

    cleanup, receive data before sending

--- clientEx.c
+++ clientEx.c
@@ -1,62 +1,53 @@
-/*
-    C ECHO client example using sockets
-*/
-#include <stdio.h> //printf
-#include <string.h>    //strlen
-#include <sys/socket.h>    //socket
-#include <arpa/inet.h> //inet_addr
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT_NUMBER 5108
  
 int main(int argc , char *argv[])
 {
-    int sock;
-    struct sockaddr_in server;
-    char message[1000] , server_reply[2000];
-     
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-     
-    server.sin_addr.s_addr = inet_addr("161.73.147.225");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 5108 );
+  int sock;
+  struct sockaddr_in server;
+  char message[1000] , server_reply[2000];
+   
+  // Create socket
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock == -1) {
+    printf("C: Could not create socket");
+  }
+  puts("C: Socket created");
+   
+  server.sin_addr.s_addr = inet_addr("161.73.147.225");
+  server.sin_family = AF_INET;
+  server.sin_port = htons( PORT_NUMBER );
  
-    //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
-        return 1;
+  // Connect to server
+  if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0) {
+    perror("C: connect failed. Error");
+    return 1;
+  }
+   
+  puts("C: Connected\n");
+   
+  // Communicate with server
+  while(1) {
+    // Receive a reply from the server
+    if ( recv(sock , server_reply , 2000 , 0) < 0) {
+      puts("C: recv failed");
+      break;
     }
-     
-    puts("Connected\n");
-     
-    //keep communicating with server
-    while(1)
-    {
-        printf("Enter message : ");
-        scanf("%s" , message);
-         
-        //Send some data
-        if( send(sock , message , strlen(message) , 0) < 0)
-        {
-            puts("Send failed");
-            return 1;
-        }
-         
-        //Receive a reply from the server
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
-         
-        puts("Server reply :");
-        puts(server_reply);
+    puts(server_reply);
+    
+    scanf("%s" , message);
+    
+    // Send data
+    if ( send(sock, message, strlen(message), 0) < 0) {
+      puts("C: Send failed");
+      return 1;
     }
-     
-    close(sock);
-    return 0;
+  }
+   
+  close(sock);
+  return 0;
 }
